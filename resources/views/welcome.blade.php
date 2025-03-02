693bba3895393c3736ff723d462fd1aafa811976
Wan Haikal <wanh5286@gmail.com>
2025-03-02 08:57:58 +0800

    initialize tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    @vite('resources/css/app.css')
     <!--<style>
                 /* General Styles */
             body {
@@ -331,6 +332,7 @@
 
     </style>-->
 </head>
+
 <body>
 <button class="toggle-btn" onclick="toggleSidebar()">☰</button>
     <nav class="sidebar" id="sidebar">
